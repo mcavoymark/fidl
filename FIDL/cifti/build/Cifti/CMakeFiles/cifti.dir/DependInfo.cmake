124dcf946d93c9dfd19d2a59eb68d9adf40a9359
Mark McAvoy <106771462+mcavoymark@users.noreply.github.com>
2022-06-02 15:42:57 -0500

    Add files via upload

---
+++ FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/CiftiBrainModelsMap.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/CiftiBrainModelsMap.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/CiftiFile.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/CiftiFile.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/CiftiLabelsMap.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/CiftiLabelsMap.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/CiftiMappingType.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/CiftiMappingType.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/CiftiParcelsMap.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/CiftiParcelsMap.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/CiftiScalarsMap.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/CiftiScalarsMap.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/CiftiSeriesMap.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/CiftiSeriesMap.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/CiftiVersion.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/CiftiVersion.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/CiftiXML.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/CiftiXML.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/Label.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/Label.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/LabelTable.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/LabelTable.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/MetaData.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/MetaData.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/StructureEnum.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/StructureEnum.cxx.o"
+  "/home/hannah/mcavoy/FIDL/cifti/CiftiLib-master/Cifti/VolumeSpace.cxx" "/home/hannah/mcavoy/FIDL/cifti/build/Cifti/CMakeFiles/cifti.dir/VolumeSpace.cxx.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "CIFTILIB_USE_QT"
+  "QT_DLL"
+  "QT_CORE_LIB"
+  "CIFTILIB_BOOST_NO_FSV3"
+  "CIFTILIB_BOOST_NO_CANONICAL"
+  "CIFTILIB_BOOST_NO_TRY_LEXICAL"
+  "CARET_HAVE_ZLIB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
