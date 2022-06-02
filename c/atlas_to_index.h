a8bac4c955f798edd54b4f1f831fdd151dcaf973
mcavoymark <106771462+mcavoymark@users.noreply.github.com>
2022-06-02 14:04:25 -0500

    Delete c directory

--- c/atlas_to_index.h
+++
@@ -1,9 +0,0 @@
-/* Copyright 10/2/15 Washington University.  All Rights Reserved.
-   atlas_to_index.h  $Revision: 1.3 $*/
-/*source af3d_xyzindex*/
-
-//START160621
-#include "get_atlas_param.h"
-
-void atlas_to_index(int ncoor,double **atlascoor,Atlas_Param *ap,int *xi,int *yi,int *zi,double *xd,double *yd,double *zd,int *index);
-void atlas_to_index2(int ncoor,double *coor,Atlas_Param *ap,int *xi,int *yi,int *zi,double *xd,double *yd,double *zd,int *index);
--- c/count_tab.h
+++
@@ -1,3 +0,0 @@
-/* Copyright 3/23/15 Washington University.  All Rights Reserved.
-   count_tab.h  $Revision: 1.2 $ */
-int count_tab(char *sp1,char *sp2,char terminator);
